6291260a819ce95c49825c77445e91f5464a201d
kylinblue <shengyinong@gmail.com>
2024-06-02 12:25:15 -0400

    Added classes

---
+++ src/main/java/com/csc340/truckshare/backend/webapp/UserRepository.java
@@ -0,0 +1,16 @@
+package com.csc340.truckshare.backend.webapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserRepository {
+
+    @Autowired
+    ListingRepository listingRepository;
+    ConversationRepository conversationRepository;
+
+    public
+}
