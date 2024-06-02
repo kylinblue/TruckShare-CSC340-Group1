d553e78743fb015f433dcc057b397e7537e0a7e1
kylinblue <shengyinong@gmail.com>
2024-06-02 12:34:39 -0400

    Users update

--- src/main/java/com/csc340/truckshare/backend/webapp/UserRepository.java
+++ src/main/java/com/csc340/truckshare/backend/webapp/UserRepository.java
@@ -1,16 +1,12 @@
 package com.csc340.truckshare.backend.webapp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 
 @Service
 public class UserRepository {
 
-    @Autowired
-    ListingRepository listingRepository;
-    ConversationRepository conversationRepository;
-
-    public
 }
