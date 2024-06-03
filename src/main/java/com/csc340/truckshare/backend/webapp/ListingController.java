1f8bdcb8faaa3c7bd4e8d10bc5f1dfc214e55f24
Tbazen00 <trevorbazen@uncg.edu>
2024-06-02 22:22:08 -0400

    Convo

--- src/main/java/com/csc340/truckshare/backend/webapp/ListingController.java
+++ src/main/java/com/csc340/truckshare/backend/webapp/ListingController.java
@@ -3,10 +3,9 @@ package com.csc340.truckshare.backend.webapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 
 @Controller
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class ListingController {
 
+    RedirectAttributes attributes;
     @Autowired UserService userService;
     @Autowired ListingService listingService;
     @Autowired ConversationService conversationService;
@@ -30,5 +30,27 @@ public class ListingController {
         return "find-Listing-By-User-Id";
     }
 
+    @PostMapping("/createListing")
+    public RedirectView createListing(Listing listing, RedirectAttributes attributes) {
+        listingService.createListing(listing);
+        attributes.addFlashAttribute("message", "Listing created successfully!");
+        return new RedirectView("/webapp/user/userid/");
+                                                            //fix address!!
+    }
+
+    @PostMapping("/updateListing")
+    public RedirectView updateListing(@RequestParam("listingId") int id, Listing listing, RedirectAttributes attributes) {
+        listingService.updateListing(id, listing);
+        attributes.addFlashAttribute("message", "Listing updated successfully!");
+        return new RedirectView("/webapp/user/userid/");
+                                                             //fix address!!
+    }
+
+    @PostMapping("/deleteListing")
+    public RedirectView deleteListing(@RequestParam("listingId") int id, RedirectAttributes attributes) {
+        listingService.deleteListing(id);
+        attributes.addFlashAttribute("message", "Listing deleted successfully!");
+        return new RedirectView("/webapp/user/userid/");
+    }
 }
 
