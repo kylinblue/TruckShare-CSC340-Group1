e3c2c29a057ef47fe528bbcf85bd2201518c53aa
Nathaniel Gebregziabher <n_gebregzia@uncg.edu>
2024-06-02 20:16:57 -0400

    repo

--- src/main/java/com/csc340/truckshare/backend/webapp/ConversationRepository.java
+++ src/main/java/com/csc340/truckshare/backend/webapp/ConversationRepository.java
@@ -1,4 +1,11 @@
 package com.csc340.truckshare.backend.webapp;
 
-public class ConversationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ConversationRepository extends JpaRepository<Conversation, Integer> {
+    @Query(value = "select * from Conversation where user_id =?1", nativeQuery = true)
+    List<Conversation> queryByUserId(int userId);
 }
